f1da7a6c55a42aec291c15380ae1e65b5f607355
Pardeep Singh <pardeep@logic-square.com>
2022-05-01 16:11:03 +0530

    package json scripts updated for heroku

--- frontend/src/config/index.js
+++ frontend/src/config/index.js
@@ -1,7 +1,5 @@
 export const BASE_URL =
-  process.env.REACT_APP_BACKEND_ENV === "live"
-    ? "https://lets-chat.herokuapp.com/api"
-    : "http://127.0.0.1:5000/api";
+  process.env.REACT_APP_BACKEND_ENV === "live" ? "/api" : "/api";
 
 export const SOCKET_BASE_URL =
   process.env.REACT_APP_BACKEND_ENV === "live"
